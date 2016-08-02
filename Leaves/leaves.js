821c3660ab96f562d0616d2c353251929528a2d3
Cyrvan BOUCHARD <cbouchard@lucca.fr>
2016-08-02 10:15:40 +0200

    [LEAVES][ADD]Changement de l'indentation du leaves.js

--- Leaves/leaves.js
+++ Leaves/leaves.js
@@ -13,14 +13,14 @@ var urlBase = 'https://lucca.ilucca.net/api/v3/leaves?leavePeriod.ownerId=greate
 var appToken = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
 console.log(urlBase + '&date=' + todayS + '&fields=isAM,leavePeriod[owner.name,endsOn,endsAM]');
 fetch(urlBase + '&date=' + todayS + '&fields=isAM,leavePeriod[owner.name,endsOn,endsAM]', {
-	'headers': {
-		'Authorization': 'lucca application=' + appToken
-	}
+  'headers': {
+    'Authorization': 'lucca application=' + appToken
+  }
 })
-	.then(function (res) {
-		return res.json();
-	}).then(function (data) {
-		console.log(data);
+  .then(function (res) {
+    return res.json();
+  }).then(function (data) {
+    console.log(data);
     var leaves = data.data.items;
     var hash = {};
     var result = [];
@@ -32,42 +32,42 @@ fetch(urlBase + '&date=' + todayS + '&fields=isAM,leavePeriod[owner.name,endsOn,
         userleave = {
           morning: false,
           afternoon: false,
-					end: leave.leavePeriod.endsOn.split('T')[0]
+          end: leave.leavePeriod.endsOn.split('T')[0]
         };
         hash[username] = userleave;
         result.push({
-					name: username,
-					leave: userleave
-				});
+          name: username,
+          leave: userleave
+        });
       }
       if (leave.isAM) {
-				userleave.morning = true;
+        userleave.morning = true;
       } else {
-				userleave.afternoon = true;
+        userleave.afternoon = true;
       }
     }
 
-		for (var i = 0; i < result.length; i++) {
-			var curRes = result[i];
-			if (curRes.leave.morning && result[i].leave.afternoon) {
-				curRes.detail = 'toute la journée';
-			} else if (curRes.leave.morning) {
-				curRes.detail = 'ce matin';
-			} else {
-				curRes.detail = 'cet après-midi';
-			}
-			if (curRes.leave.end !== todayS) {
-				var endSp = curRes.leave.end.split('-');
-				curRes.detail += ' et jusqu\'au ' + endSp[2] + '/' + endSp[1] + '/' + endSp[0] + ' inclus';
-			}
-		}
-		if (result.length === 0) {
-			result.push({
-				name: 'Personne n\'',
-				detail: 'aujourd\'hui'
-			});
-		}
-		callback(null, result);
-	}).catch(function (error) {
-		console.log(error);
-	});
+    for (var i = 0; i < result.length; i++) {
+      var curRes = result[i];
+      if (curRes.leave.morning && result[i].leave.afternoon) {
+        curRes.detail = 'toute la journée';
+      } else if (curRes.leave.morning) {
+        curRes.detail = 'ce matin';
+      } else {
+        curRes.detail = 'cet après-midi';
+      }
+      if (curRes.leave.end !== todayS) {
+        var endSp = curRes.leave.end.split('-');
+        curRes.detail += ' et jusqu\'au ' + endSp[2] + '/' + endSp[1] + '/' + endSp[0] + ' inclus';
+      }
+    }
+    if (result.length === 0) {
+      result.push({
+        name: 'Personne n\'',
+        detail: 'aujourd\'hui'
+      });
+    }
+    callback(null, result);
+  }).catch(function (error) {
+    console.log(error);
+  });
