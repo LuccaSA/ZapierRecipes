23d3ee1064b38cbbdcacf6982be3696a5e342d3c
Cyrvan BOUCHARD <cyrvan.bouchard@gmail.com>
2016-08-08 09:48:17 +0200

    [LEAVES][ADD]Modification du nombre d'appels

--- Leaves/leaves.js
+++ Leaves/leaves.js
@@ -47,32 +47,8 @@ var urlBase = getUrlBase(process.argv[2]);
 var appToken = process.argv[3];
 console.log(urlBase + '&date=' + curDay.todayS + '&fields=isAM,leavePeriod[owner.name,endsOn,endsAM]');
 
-// Permet d'obtenir la liste des personnes à vérifier et de savoir si elles sont absentes ou non
-
 var result = [];
 
 // On lance la recherche pour J+1
 
 request(result, secondStep, urlBase, curDay, appToken, when, -1);
-
-// Puis la recherche pour J+2
-
-var secondStep = function () {
-
-  var tmp = getNextWorkingDate(2);
-  var when = tmp[0];
-  var curDay = tmp[1];
-
-  request(result, thirdStep, urlBase, curDay, appToken, when, 5);
-};
-
-// Et enfin la recherche pour J+5
-
-var thirdStep = function () {
-
-  var tmp = getNextWorkingDate(5);
-  var when = tmp[0];
-  var curDay = tmp[1];
-
-  request(result, callback, urlBase, curDay, appToken, when, 10);
-}
