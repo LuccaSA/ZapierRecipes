acff8b0e3d269d61a21efd9454b3ab18e5dca6d4
Cyrvan BOUCHARD <cyrvan.bouchard@gmail.com>
2016-08-09 10:34:43 +0200

    [LEAVE][FIX]Correction d'un bug du leave

--- Leaves/leaves.js
+++ Leaves/leaves.js
@@ -1,25 +1,23 @@
 // Fonction qui permet à partir du paramètre params de retourner l'url qui sera utilisé.
 
-(function () {
-  function getUrlBase(params) {
-    if (!params) {
-      return null;
-    }
-    var endUrlBase = '/api/v3/leaves?leavePeriod.ownerId=greaterthan,0';
-
-    if (!params.match('^https://')) {
-      params = 'https://' + params;
-    }
-    if (!params.match('(.ilucca.net)$')) {
-      params = params + '.ilucca.net';
-    }
-    return (params + endUrlBase);
+function getUrlBase(params) {
+  if (!params) {
+    return null;
   }
+  var endUrlBase = '/api/v3/leaves?leavePeriod.ownerId=greaterthan,0';
 
-  var nextDay = getNextWorkingDate(input.offsetDays);
-  if (input && nextDay) {
-    getAlreadyLeave(nextDay);
+  if (!params.match('^https://')) {
+    params = 'https://' + params;
   }
-})();
+  if (!params.match('(.ilucca.net)$')) {
+    params = params + '.ilucca.net';
+  }
+  return (params + endUrlBase);
+}
+
+var nextDay = getNextWorkingDate(input.offsetDays);
+if (input && nextDay) {
+  getAlreadyLeave(nextDay);
+}
 
 
