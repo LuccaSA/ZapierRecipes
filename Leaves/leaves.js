ffa9b8966c912e3cb3697c10fac2c872cb334be3
Cyrvan BOUCHARD <cyrvan.bouchard@gmail.com>
2016-08-10 13:47:12 +0200

    [LEAVES][ADD]Ajout de vérification pour les variables prisent en entrée

--- Leaves/leaves.js
+++ Leaves/leaves.js
@@ -3,6 +3,14 @@ var nextDay = getNextWorkingDate(input.offsetDays);
 // Permet de lancer le script une fois quand les variables existent
 
 if (input && nextDay) {
+  if (input.offsetDays === undefined || input.offsetDays < 0) {
+    input.offsetDays = 0;
+  }
+
+  if (input.numberDayMinimum === undefined || input.offsetDays < 0) {
+    input.offsetDays = 0;
+  }
+
   getAlreadyLeave(nextDay);
 }
 
