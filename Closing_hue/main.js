8da31c625e0d7b8cc992b711c33c253f3f2502f7
Cyrvan BOUCHARD <cyrvan.bouchard@gmail.com>
2016-11-18 17:57:03 +0100

    [HUE][ADD] Détection du bridge et lancement de la colorloop sous la détéction du message 'nouveau_closing'

--- Closing_hue/main.js
+++ Closing_hue/main.js
@@ -1,44 +1,62 @@
 var token = process.env.token;
 //////////////////////////////////////
 
-(function () {
-    var fetch = require('node-fetch');
-    var RtmClient = require('@slack/client').RtmClient;
-    var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var stopColorLoop = function (bridgeLocalIp) {
+    const fetch = require('node-fetch');
 
-    var rtm = new RtmClient(token);
-    rtm.start();
+    var url = 'http://' + bridgeLocalIp + '/api/ovpiVwMuf51lWFMh/groups/1/action';
+    var body = { method: 'PUT', body: JSON.stringify({'effect': 'none'})};
 
-    var callback = function (error, success) {
-        console.log(success);
-    }
+    fetch(url, body).catch((error) => console.log(error));
+};
 
-    var hueController = {}
+var launchColorLoop = function (bridgeLocalIp) {
+    const fetch = require('node-fetch');
 
-    hueController.url = process.env.url;
+    var url = 'http://' + bridgeLocalIp + '/api/ovpiVwMuf51lWFMh/groups/1/action';
+    var body = { method: 'PUT', body: JSON.stringify({'effect': 'colorloop'})};
 
-    hueController.sendRequest = function (url, init) {
-        fetch(url, init)
-            .catch(function (error) { console.log(error); });
-    }
+    fetch(url, body).then(function () {
+        setTimeout(function () {
+            stopColorLoop(bridgeLocalIp);
+        }, 15000);
+    }).catch((error) => console.log(error));
+};
 
-    hueController.sendRequest(hueController.url, { method: 'PUT', body: JSON.stringify({ xy:[0.3, 0.3] , bri: 254 }) });
+var launchSlackListerner = function (bridgeLocalIp) {
+    const SlackBot = require('slackbots');
 
-    hueController.setEffect = function (effect) {
-        var init = {
-            method: 'PUT',
-            body: JSON.stringify({
-                effect: effect
-            })
-        };
-        this.sendRequest(this.url, init);
-    }
+    const channel = 'C22UV12R5'; // It's the 'nouveau_closing' channel's id from the slackbutton slack team.
+    const text = 'nouveau_closing'; // The expected message to trigger the hue lamps
 
-    rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-        if (message.channel === process.env.channel) {
-            hueController.setEffect('colorloop');
-            setTimeout(function () { hueController.setEffect('none'); }, 20000);
-        }
+    // Create a bot
+    var bot = new SlackBot({
+        token: token
     });
 
-})();
+    bot.on('message', function (data) {
+        if (data !== undefined && data.channel === channel && data.text === text) {
+            launchColorLoop(bridgeLocalIp);
+        }
+    });
+};
+
+var getBridgeLocalIp = function () {
+    var fetch = require('node-fetch');
+
+    fetch('https://www.meethue.com/api/nupnp')
+        .then(result => result.json())
+        .then(function (data) {
+            if (data[0] !== undefined && data[0].internalipaddress !== undefined) {
+                console.log('Launched on ' + data[0].internalipaddress + ' .');
+                launchSlackListerner(data[0].internalipaddress);
+            } else {
+                console.log('Can not found any bridge. Please try in another network.');
+            }
+        })
+        .catch(error => console.log(error));
+};
+
+(function () {
+    getBridgeLocalIp();
+})()
